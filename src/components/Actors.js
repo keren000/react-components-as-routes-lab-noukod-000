c71df124a913416f1c66d99159c0d5f94e293c62
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-22 13:59:26 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -6,6 +6,16 @@ const Actors = () => {
     <div class="actors">
       {/*{code here}*/}
       <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h3>{actor.name}</h3>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
