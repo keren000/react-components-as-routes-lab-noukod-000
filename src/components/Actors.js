ef613a0c28ae8052801a0029e2aea3f5b7686260
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-22 13:54:15 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,8 +3,9 @@ import { actors } from '../data';
 
 const Actors = () => {
   return (
-    <div>
+    <div class="actors">
       {/*{code here}*/}
+      <h1>Actors Page</h1>
     </div>
   );
 };
